02e9a8376d590d2734743feb27e3850d879540cb
Jordan Leslie <this.codedev@gmail.com>
2017-09-18 15:22:00 -0500

    Solution for first java exercise

---
+++ src/java_syntax.java
@@ -0,0 +1,27 @@
+public class java_syntax {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 6;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Welcome to Westworld";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+//        String class = "hi";
+
+//        int x = 4;
+//        x += 5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+//        x  /= y;
+//        y -= x;
+        System.out.println(y-=x);
+    }
+}
