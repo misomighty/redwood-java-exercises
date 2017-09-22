41bff1989c9bb230c3fc115be2998bb80eb7f3d7
Jordan Leslie <this.codedev@gmail.com>
2017-09-22 16:06:38 -0500

    Add Fer walkthrough, changed multiple scanner declations to 1 declaration passed as a parameter

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -3,12 +3,13 @@ import java.lang.Math;
 public class MethodsExercise {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         // CALL DICE
-        diceRoll();
+        diceRoll(scan);
         // CALL FAC
-        System.out.println(factorial());
+        System.out.println(factorial(scan));
         // CALL GET INT
-        System.out.println(getInteger(1, 10));
+        System.out.println(getInteger(1, 10, scan));
         // CALL MOD
         System.out.println(modulus(10, 5));
         // CALL MUL
@@ -24,11 +25,11 @@ public class MethodsExercise {
         // CALL HELLO
         sayHello("Jordan", 18);
         // RECURSION
-        for(int i = 0; i <= 10; i++) {
-            System.out.println("i = " +i);
-        }
-
-        System.out.println(counter(0));
+//        for(int i = 0; i <= 10; i++) {
+//            System.out.println("i = " +i);
+//        }
+//
+//        System.out.println(counter(0));
     }
     //You can have two different versions of methods with the same name as long as the parameters are different
     //The program will determine if parameters are the same by 1) # of parameters and 2) data types
@@ -69,22 +70,21 @@ public class MethodsExercise {
         return counter(i);
     }
 
-    public static long getInteger(int min, int max) {
-        Scanner scan = new Scanner(System.in);
+    public static long getInteger(int min, int max, Scanner scan) {
+
         System.out.println("Let's get the integer of a number between 1 and 10: ");
         int userInput = scan.nextInt();
 
         if(userInput >= min && userInput <= max) {
             return userInput;
         } else {
-            return getInteger(min, max);
+            return getInteger(min, max, scan);
         }
     }
 
-    public static long factorial() {
-        Scanner scan = new Scanner(System.in);
+    public static long factorial(Scanner scan) {
         System.out.println("Want to know the factorial of a number? (1-10): ");
-
+        // FER used factorial(getInteger(1, 10) to return a number;
         long userInput = scan.nextInt();
 
         if(userInput >= 1 && userInput <= 10) {
@@ -92,24 +92,26 @@ public class MethodsExercise {
 
             for(int i = 1; i <= userInput; i++) {
                 answer *= i;
+                // details += (i == 1) ? i: " x " + i;
+                // System.out.println( i + "! = " + details + " = " + ( acum *= i ) );
             }
             System.out.println(answer);
         }
             System.out.println("Would you like to do another: y/n ?");
             String userChoice = scan.next();
             if(userChoice.equals("y") || userChoice.equals("y".toUpperCase())){
-                return userInput = factorial();
+                return userInput = factorial(scan);
             } else {
                 return 0;
             }
     }
 
-    public static void diceRoll() {
+    public static void diceRoll(Scanner scan) {
 
         // Ask the user if they would like to roll
         while(true) {
-            if(rollOrQuit().equalsIgnoreCase("roll")) {
-                System.out.println(rollResult());
+            if(rollOrQuit(scan).trim().equalsIgnoreCase("roll")) {
+                System.out.println(rollResult(scan));
             } else {
                 break;
             }
@@ -118,24 +120,22 @@ public class MethodsExercise {
 
 
     }
-    public static int getDieSides() {
-        Scanner scan = new Scanner(System.in);
+    public static int getDieSides(Scanner scan) {
         System.out.println("How many sides do your dice have? ");
         int userInput = scan.nextInt();
         scan.nextLine();
         return userInput;
     }
 
-    public static String rollOrQuit() {
+    public static String rollOrQuit(Scanner scan) {
         // Returns user input: roll or quit
-        Scanner scan = new Scanner(System.in);
         System.out.println("Enter: Roll or Quit: [roll]/[quit]");
         String userChoice = scan.nextLine();
         return userChoice;
     }
 
-    public static String rollResult() {
-        int sides = getDieSides();
+    public static String rollResult(Scanner scan) {
+        int sides = getDieSides(scan);
         int die1 = (int) Math.floor(Math.random() * sides + 1);
         int die2 = (int) Math.floor(Math.random() * sides + 1);
         return "You rolled " +die1+ " and " +die2+ ".";
