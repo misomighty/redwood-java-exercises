80627363321f64afe97980b936a3acee7412326d
Jordan Leslie <this.codedev@gmail.com>
2017-09-26 11:30:31 -0500

    changed 3.14 to Math.PI and added comments

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,27 +2,6 @@ package shapes;
 import util.Input;
 
 public class Circle {
-    private double radius;
-    public static int numberOfCircles;
-
-    public Circle(double radius) {
-        this.radius = radius;
-        numberOfCircles++;
-
-    }
-
-    public double getArea() {
-    // area = pi x (radius ^ 2)
-        return (3.14 * Math.pow(radius, 2));
-    }
-
-    public double getCircumference() {
-    // cir = 2piR
-        return (2 * 3.14 * radius);
-    }
-
-    public void makeACircle() {}
-
 
     public static void main(String[] args) {
         Input input = new Input();
@@ -31,5 +10,26 @@ public class Circle {
         System.out.println(circle.getCircumference());
 
     }
+    private double radius;
+    public static int numberOfCircles;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        numberOfCircles++;
+    }
+
+    public double getArea() {
+    // area = pi x (radius ^ 2)
+        return (Math.PI * Math.pow(radius, 2));
+    }
+
+    public double getCircumference() {
+    // cir = 2 * pi * radius
+        return (2 * Math.PI * radius);
+    }
+
+    public int getNumberOfCircles() {
+        return numberOfCircles;
+    };
 }
 
